f13037d2be65e5dcbcd19b6f0acfb4e277baf6df
Theron Boerner <hunterboerner@gmail.com>
2013-07-08 15:29:57 -0500

    dfghjkl;'

--- AdvancedJava/Minecraft/BukkitTutorial/src/com/dma/tutorialplugin/TowerGen.java
+++ AdvancedJava/Minecraft/BukkitTutorial/src/com/dma/tutorialplugin/TowerGen.java
@@ -10,8 +10,11 @@ import org.bukkit.block.Block;
  * Date: 7/8/13
  * Time: 2:30 PM
  */
-public class TowerGen{
-    private final SampleBukkitPlugin plugin;
+
+public class TowerGen {
+    protected final SampleBukkitPlugin plugin;
+    World world;
+    int z_operate, y_operate, x_operate;
     private Location pLoc;
     private int pLength;
 
@@ -19,12 +22,11 @@ public class TowerGen{
         this.plugin = plugin;
     }
 
-
     public void generateTower(Location loc, int length) {
         // int coords[][][] = new int[length][length][length];
         pLoc = loc;
         pLength = length;
-        World world = loc.getWorld();
+        world = loc.getWorld();
         int x_start = loc.getBlockX();
         // Set the startpoints to the coordinates of the given location
         int y_start = loc.getBlockY();
@@ -54,7 +56,7 @@ public class TowerGen{
                 new Runnable() {
 
                     public void run() {
-                        World world = pLoc.getWorld();
+                        world = pLoc.getWorld();
                         int x_start = pLoc.getBlockX();
                         // Set the start points to the coordinates of the given location
                         int y_start = pLoc.getBlockY();
@@ -64,17 +66,23 @@ public class TowerGen{
                         int x_length = x_start + pLength;    // now i set the lengths for each dimension... should be clear.
                         int y_length = y_start + pLength;
                         int z_length = z_start + pLength;
-                        for (int x_operate = x_start; x_operate <= x_length; x_operate++) {
+                        for (x_operate = x_start; x_operate <= x_length; x_operate++) {
                             // Loop 1 for the X-Dimension "for x_operate (which is set to x_start)
                             //do whats inside the loop while x_operate is
                             //<= x_length and after each loop increase
                             //x_operate by 1 (x_operate++ is the same as x_operate=x_operate+1;)
-                            for (int y_operate = y_start; y_operate <= y_length; y_operate++) {// Loop 2 for the Y-Dimension
-                                for (int z_operate = z_start; z_operate <= z_length; z_operate++) {// Loop 3 for the Z-Dimension
+                            for (y_operate = y_start; y_operate <= y_length; y_operate++) {// Loop 2 for the Y-Dimension
+                                for (z_operate = z_start; z_operate <= z_length; z_operate++) {// Loop 3 for the Z-Dimension
+                                    plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+                                        public void run() {
+                                            System.out.println("OH MA GERD");
+                                            // get the block with the current coordinates
+                                            System.out.println(x_operate + "|" + y_operate + "|" + z_operate);
+                                            Block blockToChange = world.getBlockAt(x_operate, y_operate, z_operate);
+                                            blockToChange.setTypeId(0);    // set the block to Type 0 (air)
+                                        }
+                                    }, 5L);
 
-                                    // get the block with the current coordinates
-                                    Block blockToChange = world.getBlockAt(x_operate, y_operate, z_operate);
-                                    blockToChange.setTypeId(0);    // set the block to Type 0 (air)
                                 }
                             }
                         }
