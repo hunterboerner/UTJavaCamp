2a6bd90ebcac4e26acf5640c931317c14267cd32
Theron Boerner <hunterboerner@gmail.com>
2013-07-08 10:20:37 -0500

    Update

---
+++ MCPlugin/src/me/hunterboerner/main/main.java
@@ -0,0 +1,15 @@
+package me.hunterboerner.main;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: theron
+ * Date: 7/8/13
+ * Time: 9:55 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class main extends JavaPlugin{
+
+}
